2280c525af04c7b901eb331af46c7d274d897fed
BlackZan1 <kenshin.nakamura.01@mail.ru>
2019-12-17 16:25:07 +0300

    Updates

---
+++ precache-manifest.4a5dd8faf3dc5c4b4a50b3d50dd5fe6d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac22c5893f4e0baee536b633c548ded",
+    "url": "/FFSpectrum/index.html"
+  },
+  {
+    "revision": "3419ce2af3dec390fc57",
+    "url": "/FFSpectrum/static/css/main.2e460b99.chunk.css"
+  },
+  {
+    "revision": "d106883f54d10f3a606d",
+    "url": "/FFSpectrum/static/js/2.4d6cd21e.chunk.js"
+  },
+  {
+    "revision": "3419ce2af3dec390fc57",
+    "url": "/FFSpectrum/static/js/main.e2ce8748.chunk.js"
+  },
+  {
+    "revision": "9929e199f8412ca56180",
+    "url": "/FFSpectrum/static/js/runtime-main.3240c2aa.js"
+  },
+  {
+    "revision": "52970dad4dfaa962205eb06ca91d7abc",
+    "url": "/FFSpectrum/static/media/FFFONT.52970dad.otf"
+  },
+  {
+    "revision": "4bdeceabe112b30de091e771d85b7b61",
+    "url": "/FFSpectrum/static/media/bg.4bdeceab.jpg"
+  },
+  {
+    "revision": "6bc5527a5783252f9064de6e7617bf99",
+    "url": "/FFSpectrum/static/media/block-underline.6bc5527a.png"
+  },
+  {
+    "revision": "b940efa65eebc3a6f0d5686ad969ba72",
+    "url": "/FFSpectrum/static/media/logo.b940efa6.png"
+  },
+  {
+    "revision": "43a6a4f8b88238f5981791fbb59f58a3",
+    "url": "/FFSpectrum/static/media/moogle.43a6a4f8.png"
+  },
+  {
+    "revision": "945f33b643f2ceffcdae90fb57c61854",
+    "url": "/FFSpectrum/static/media/userWP.945f33b6.png"
+  },
+  {
+    "revision": "0b26d3ac2747d1756c45d3907ac28305",
+    "url": "/FFSpectrum/static/media/wild-bg.0b26d3ac.jpg"
+  }
+]);
